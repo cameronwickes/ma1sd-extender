683cd46ce7ca338f73379e3dac7903854ca81685
Cameron <cameronjwickes@gmail.com>
2022-04-21 15:01:56 +0100

    Speed Improvements

--- api/ma1sd-extender/search.py
+++ api/ma1sd-extender/search.py
@@ -1,17 +1,15 @@
 from fastapi import Request, Header
 from fastapi.responses import JSONResponse
-import requests
-import os
+from os import getenv
+from requests import get, post
+from json import loads
+from itertools import chain
 import asyncio
-import json
 
 
 async def findUsers(request: Request):
     # Declare constants
-    MA1SD_EXTENDER_USERNAME = os.getenv("MA1SD_EXTENDER_USERNAME", "")
-    MA1SD_EXTENDER_PASSWORD = os.getenv("MA1SD_EXTENDER_PASSWORD", "")
-    MA1SD_EXTENDER_FEDERATED_DOMAINS = json.loads(os.getenv("MA1SD_EXTENDER_FEDERATED_DOMAINS", "[]").replace('\'', '"'))
-    MA1SD_EXTENDER_MATRIX_DOMAIN = os.getenv("MA1SD_EXTENDER_MATRIX_DOMAIN", "")
+    MA1SD_EXTENDER_USERNAME, MA1SD_EXTENDER_PASSWORD, MA1SD_EXTENDER_FEDERATED_DOMAINS, MA1SD_EXTENDER_MATRIX_DOMAIN = getenv("MA1SD_EXTENDER_USERNAME", ""), getenv("MA1SD_EXTENDER_PASSWORD", ""), loads(getenv("MA1SD_EXTENDER_FEDERATED_DOMAINS", "[]").replace('\'', '"')), getenv("MA1SD_EXTENDER_MATRIX_DOMAIN", "")
     MA1SD_EXTENDER_ACCESS_TOKEN = getAccessToken(MA1SD_EXTENDER_MATRIX_DOMAIN, MA1SD_EXTENDER_USERNAME, MA1SD_EXTENDER_PASSWORD)
 
     # Return bad response if credentials are wrong.
@@ -25,91 +23,52 @@ async def findUsers(request: Request):
     # Extract the search term from the request body or return 400 client error.
     parsedBody = ""
     try:
-        parsedBody = json.loads(body)
-        searchTerm = parsedBody["search_term"]
+        parsedBody = loads(body)
     except:
         return JSONResponse(status_code=400, content={'errcode': 'M_UNKNOWN', 'error': 'search_term` is required field', 'success': False})
 
-    # Check if to recurse or not.
-    recurse = True
-    if "no_recursion" in parsedBody.keys():
-        recurse = False
-
     # Check access token is valid with a federation domain.
-    validAccessToken = False
-    for domain in MA1SD_EXTENDER_FEDERATED_DOMAINS:
-        if checkAccessToken(domain, headers):
-            validAccessToken = True
-    if checkAccessToken(MA1SD_EXTENDER_MATRIX_DOMAIN + ':4343', headers):
-        validAccessToken = True
+    validAccessToken = any([checkAccessToken(domain, headers) for domain in chain(MA1SD_EXTENDER_FEDERATED_DOMAINS, [f"{MA1SD_EXTENDER_MATRIX_DOMAIN}:4343"])])
 
     # Return if the access token provided is not valid for any federation domain.
     if not validAccessToken:
         return JSONResponse(status_code=400, content={"errcode": "M_UNKNOWN_TOKEN", "error": "Invalid macaroon passed.", "soft_logout": False})
 
-    directoryListings = []
-
-    # Add the local directory listing.
-    directoryListings.append(getDirectorySearch('http', 'localhost:8090', f"Bearer {MA1SD_EXTENDER_ACCESS_TOKEN}", parsedBody))
+    # Retrieve local results.
+    localDirectoryListings = [getDirectorySearch('http', 'localhost:8090', f"Bearer {MA1SD_EXTENDER_ACCESS_TOKEN}", parsedBody)]
 
     # Do recursive directory listing if requested.
-    if recurse:
+    if "no_recursion" not in parsedBody.keys():
         parsedBody["no_recursion"] = True
-        for domain in MA1SD_EXTENDER_FEDERATED_DOMAINS:
-            directoryListings.append(getDirectorySearch('https', domain, headers["authorization"], parsedBody))
+        remoteDirectoryListings = [getDirectorySearch('https', domain, headers["authorization"], parsedBody) for domain in MA1SD_EXTENDER_FEDERATED_DOMAINS]
 
     # Get final response and return it.
-    response = parseDirectoryListings(directoryListings)
-    return JSONResponse(status_code=200, content=response)
+    return JSONResponse(status_code=200, content=parseDirectoryListings(chain(localDirectoryListings, remoteDirectoryListings)))
 
 
-def getAccessToken(domain, username, password):
+async def getAccessToken(domain, username, password):
     try:
-        # Gain access token.
-        url = f"https://{domain}:4343/_matrix/client/r0/login"
-        body = {"type": "m.login.password", "identifier": {"type": "m.id.user", "user": f"{username}"}, "password": f"{password}"}
-
-        # Make request.
-        response = requests.post(url, json=body)
-        # Return result.
-        if response.status_code == 200:
-            return response.json()["access_token"]
-        else:
-            return "M_FORBIDDEN"
+        # Gain access token.and make request.
+        url, body = f"https://{domain}:4343/_matrix/client/r0/login", {"type": "m.login.password", "identifier": {"type": "m.id.user", "user": f"{username}"}, "password": f"{password}"}
+        return post(url, json=body).json()["access_token"]
     except:
         return "M_FORBIDDEN"
 
-
-def checkAccessToken(domain, headers):
+async def checkAccessToken(domain, headers):
     try:
         # Validate access token.
-        url = f"https://{domain}/_matrix/client/r0/account/3pid"
-        headers = {"Authorization": headers["authorization"]}
-        print(url)
-        print(headers)
-        response = requests.get(url, headers=headers)
-        print(response)
-
-        # Return result.
-        if response.status_code == 200:
-            return True
-
-        # Return false if no OK status code.
-        return False
+        url, headers = f"https://{domain}/_matrix/client/r0/account/3pid", {"Authorization": headers["authorization"]}
+        return get(url, headers=headers).status_code == 200
     except:
         return False
 
 
-def getDirectorySearch(https, domain, token, body):
+async def getDirectorySearch(https, domain, token, body):
     try:
         # Set headers.
-        url = f"{https}://{domain}/_matrix/client/r0/user_directory/search"
-        headers = {"Authorization": token}
-        print(url, headers)
+        url, headers = f"{https}://{domain}/_matrix/client/r0/user_directory/search", {"Authorization": token}
+        response = post(url, json=body, headers=headers)
 
-        # Get response and return it.
-        response = requests.post(url, json=body, headers=headers)
-        print(response.json())
         if response.status_code == 200 and "error" not in response.json().keys():
             return response.json()
         return []
@@ -117,17 +76,8 @@ def getDirectorySearch(https, domain, token, body):
         return []
 
 
-def parseDirectoryListings(directoryListings):
-    directory = {"limited": False, "results": []}
+async def parseDirectoryListings(directoryListings):
     try:
-        for dir in directoryListings:
-            # Set limited to true if one is true.
-            print(dir)
-            if dir["limited"] == True:
-                directory["limited"] = True
-            # Append the results together.
-            directory["results"] += dir["results"]
-
-        return directory
+        return {"limited": any([x["limited"] for x in directoryListings]), "results": chain(*[x["results"] for x in directoryListings])}
     except:
-        return directory
+        return {"limited": False, "results": []}
